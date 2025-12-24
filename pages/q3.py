326f12900c375d3b8d30f2fc3c570f3815d5125b
lyzzyl1 <937923348@qq.com>
2025-12-24 15:40:53 +0800

    Update q3.py

--- pages/q3.py
+++ pages/q3.py
@@ -40,7 +40,7 @@ st.markdown("""
 # 2. 图片
 st.subheader("🔬 生态系统能量金字塔示意图")
 st.image(
-    "./pages/p3.PNG",
+   Path(file).parent / "p3.PNG",
     caption="图1：典型的生态系统能量金字塔，显示能量在营养级间的递减关系",
     use_container_width=True
 )
@@ -95,6 +95,7 @@ with col_right:
         st.session_state.history = []
 
         st.switch_page("pages/q3.py")
+
 
 
 
